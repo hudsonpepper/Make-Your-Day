0e6afc68ab14d23bbfeca0875b519df6686701e2
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2023-12-08 14:18:27 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,21 +29,6 @@ var monthArr = [31,28,31,30,31,30,31,31,30,31,30,31];
 console.log(currMonth);
 var monthLen = monthArr[currMonth-1];
 
-//Var of each month
-
-var jan = 31;
-var feb = 28; // Note: 29 in a leap year
-var mar = 31;
-var apr = 30;
-var may = 31;
-var jun = 30;
-var jul = 31;
-var aug = 31;
-var sep = 30;
-var oct = 31;
-var nov = 30;
-var dec = 31;
-
 
 
 var todos = [];
