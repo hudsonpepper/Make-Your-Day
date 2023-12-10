9d35f39b362fcf517818d5eeb343ceb9ad3b8280
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2023-12-10 14:09:51 -0500

    Added Accordion

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,6 +161,26 @@ day.each(function() {
   }
 });
 
+// Accordion 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+    console.log("click");
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+};
+
 // Make the day event list sortable and connected to the todo lists
 
 
